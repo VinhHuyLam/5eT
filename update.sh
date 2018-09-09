0a98d9e6495ee3d18acffbbe294f91faf262a11c
Vincent <VinhHuyLam@users.noreply.github.com>
2018-09-09 17:24:55 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,11 +11,12 @@ if [ ! -d $LOCALREPO_VC_DIR ]
 then
     echo Cloning repository... 
     git clone $REPOSRC $LOCALREPO
+    npm run dev-server
 else
     echo Repository exists: updating repository... 
     cd $LOCALREPO
     git pull $REPOSRC
+    npm run dev-server
 fi
 
 # End
-exec "$@"
